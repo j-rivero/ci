a52dd429fa2d223179da18f0b9ab17f04ab2f3ca
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-03-02 17:34:56 +0100

    Fix syntax ini

--- ros2_batch_job/__main__.py
+++ ros2_batch_job/__main__.py
@@ -379,7 +379,7 @@ def build_and_test(args, job):
 
     # xunit2 format is needed to make Jenkins xunit plugin 2.x happy
     pytest_ini = open("pytest.ini", "w")
-    pytest_ini.write("[pytest]\n junit_family=xunit2\n")
+    pytest_ini.write("[pytest]\njunit_family=xunit2")
     pytest_ini.close()
 
     test_cmd = [
